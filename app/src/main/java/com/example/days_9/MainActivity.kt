88662173a6823c3f32b9ca967efac8ac9f659b09
Mehmet Can Gündüz <gunduzm410@gmail.com>
2023-05-01 19:12:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/days_9/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.days_9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val xml = XmlResult()
+
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val arr = xml.xmlCatalog()
+        for ( item in arr ) {
+            Log.d("item", item.ZONE)
+        }
+        /*
+        Thread{
+            xml.xmlCatalog()
+        }.start()
+*/
+/*
+GlobalScope.launch {
+    xml.xmlCatalog()
+}
+*/
+Log.d("line","This Line")
+
+
+
+
+
+
+
+
+
+
+}
+}
